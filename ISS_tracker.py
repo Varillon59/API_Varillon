9e8fe96684118ce83c3a9418ed7d736279276571
Varillon59 <91702318+Varillon59@users.noreply.github.com>
2021-12-17 22:18:58 +0100

    Update ISS_tracker.py

--- ISS_tracker.py
+++ ISS_tracker.py
@@ -1,17 +1,19 @@
 import requests
 import json
+from time import sleep
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
-
-
-if response.status_code == 200:
-    print("Success.")
-else:
-    print("An error has occurred.")
-
-iss=response.json()
-
-Longitude=iss['iss_position']['longitude']
-Latitude=iss['iss_position']['latitude']
-
-print("L'ISS se trouve au coordonnées:", Longitude, "/", Latitude)
+while True:
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    
+    if response.status_code == 200:
+        print("Success.")
+    else:
+        print("An error has occurred.")
+        
+    iss=response.json()
+        
+    Longitude=iss['iss_position']['longitude']
+    Latitude=iss['iss_position']['latitude']
+        
+    print("L'ISS se trouve au coordonnées:", Longitude, "/", Latitude)
+    sleep(5)
